fb907e267bd497f6c817318bde634631e71dbd31
Caleb Atwood <calebwilliamatwood@gmail.com>
2015-03-12 14:22:44 -0600

    Add tweet_fetcher.rb model

---
+++ app/models/tweet_fetcher.rb
@@ -0,0 +1,19 @@
+class TweetFetcher
+
+
+  def initialize(twitter_handle)
+    @twitter_handle = twitter_handle
+
+    @client = Twitter::REST::Client.new do |config|
+      config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"]
+      config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]
+      config.access_token        = ENV["TWITTER_ACCESS_KEY"]
+      config.access_token_secret = ENV["TWITTER_ACCESS_SECRET"]
+    end
+  end
+
+  def new_tweet
+    @client.update(@twitter_handle)
+  end
+
+end
